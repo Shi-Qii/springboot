846d3bc60bd42c0494a9b73b260fe8a51d024375
Shi-Qi <qweqwe0528@gmail.com>
2022-01-23 10:23:16 +0800

    新增 使用專案路徑讀檔案

--- src/main/java/com/example/demo/jython/test_python.java
+++ src/main/java/com/example/demo/jython/test_python.java
@@ -1,31 +1,38 @@
 package com.example.demo.jython;
 
-import jnr.posix.WString;
-import org.python.core.PyFunction;
-import org.python.core.PyObject;
-import org.python.core.PyString;
-import org.python.util.PythonInterpreter;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.*;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 
 public class test_python {
-    public static void main(String[] args) {
+
+
+
+    public static void main(String[] args) throws IOException {
+
         String par1="Listed_Foreign_Buy_Day";
         String par2="上市";
         String par3="Buy";
         String par4="Foreign_investors";
         String par5= "20";
-        String[] arguments = new String[] {"python", "F:\\MY_STOCK_WEB\\springboot\\src\\main\\resources\\python_api\\institutional_investors.py", par1,par2,par3,par4,par5};
+
+        String id = "institutional_investors"+".py"; //到時候從前端傳進來對應的名子
+
+        String pyClasspathTemplate = "src/main/resources/python_api/%s";
+        String pyFolderClasspathPath = String.format(pyClasspathTemplate, id);
+        String filePath = new File(pyFolderClasspathPath).getAbsolutePath();
+
+        String[] arguments = new String[] {"python", filePath, par1,par2,par3,par4,par5};
         String table = "";
         try {
             Process process = Runtime.getRuntime().exec(arguments);
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(),"utf-8"));
+            InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream(), Charset.forName("utf-8"));
+            String readerEncoding = inputStreamReader.getEncoding();
+            BufferedReader in = new BufferedReader(inputStreamReader);
             String line = null;
             while ((line = in.readLine()) != null) {
+
                 table += line;
                 System.out.println(table);
             }
