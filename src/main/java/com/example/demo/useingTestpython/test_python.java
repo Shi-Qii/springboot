7bf0e3826eceecd8aab4c29c5e5e87abbd4f19a0
billy <billy06032002@gmail.com>
2022-01-24 23:23:07 +0800

    調整API產出json格式 新增月營收API

--- src/main/java/com/example/demo/useingTestpython/test_python.java
+++ src/main/java/com/example/demo/useingTestpython/test_python.java
@@ -11,8 +11,8 @@ public class test_python {
 
     public static void main(String[] args) throws IOException {
         System.out.println("建立develop");
-        String par1="Ind_Monthly_Revenue_Mon";
-        String par2="2330";
+        String par1="Listed_Monthly_Revenue";
+        String par2="上市";
         String par3="2330";
         String par4="Foreign_investors";
         String par5= "20";
