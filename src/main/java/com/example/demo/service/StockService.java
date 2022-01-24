0efd951d09122481a9dd6121d49f72ed044f5e4a
Shi-Qi <qweqwe0528@gmail.com>
2022-01-24 21:39:32 +0800

    新增 修改 全端串接

--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Institutional;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -22,14 +23,15 @@ public class StockService {
 //    Institutional institutional;
 
 
-    public Map<String, ArrayList<Object>> getData() throws IOException {
-
+    public Map<String, ArrayList<Object>> getData(@RequestBody Map<String, Object> requestMap) throws IOException {
+        System.out.println("查看map:" + requestMap);
+        String pythonKey = (String) requestMap.get("key");
         HashMap<String, Map<String, Object>> objectObjectHashMap = new HashMap<>();
         HashMap<String, ArrayList<Object>> objectObjectHashMap2 = new HashMap<>();
         String path = getPath();
         String table = "";
         HashMap<String, String> objectHashMap = new HashMap<>();
-        objectHashMap.put("parameter1", "Ind_Institutional_Investors_Day");
+        objectHashMap.put("parameter1", pythonKey);
         objectHashMap.put("parameter2", "1110");
         objectHashMap.put("parameter3", "10");
         objectHashMap.put("parameter4", "Foreign_investors");
